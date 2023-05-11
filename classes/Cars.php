d8a89607b29825614aadd9be338108b09a5c753d
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-05-11 11:19:29 +0200

    update cars.php

--- classes/Cars.php
+++ classes/Cars.php
@@ -2,9 +2,9 @@
 
 class cars {
 
-    public static function loadcars($filter = "")
+    public static function loadcars($filter = "", $sortby = "Nummer" , $orderby = "ASC")
     {
-        $results = database::getRows('autoverzameling' ,['Model', 'Merk', 'Nummer','Producent', 'Modelnr'], 'sssss', ['%' . $filter . '%','%' . $filter . '%','%' . $filter . '%','%' . $filter . '%','%' . $filter . '%'], "JOIN `staat voertuig` ON autoverzameling.staat=`staat voertuig`.id join locatie on autoverzameling.`huidige locatie`=locatie.id", 'Nummer');
+        $results = database::getRows('autoverzameling' ,['Model', 'Merk', 'Nummer','Producent', 'Modelnr'], 'sssss', ['%' . $filter . '%','%' . $filter . '%','%' . $filter . '%','%' . $filter . '%','%' . $filter . '%'], "JOIN `staat voertuig` ON autoverzameling.staat=`staat voertuig`.id join locatie on autoverzameling.`huidige locatie`=locatie.id", $sortby . " " . $orderby);
         if ($results) {
             return $results;
         } else {
